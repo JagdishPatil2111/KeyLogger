012633fd6f4193019e4e76eae188d1b36746e3bf
Jagdish Patil <78476615+JagdishPatil2111@users.noreply.github.com>
2021-12-20 22:35:31 +0530

    Program to Generate Encryption Key
    This program generates an encryption key that initiates the process of keylogging and Decrypting the received data to make it readible.

--- Encryption files/GenerateKey.py
+++ Encryption files/GenerateKey.py
@@ -1,6 +1,6 @@
 from cryptography.fernet import Fernet
 
-key = Fernet.generate_key()
+key = Fernet.generate_key() # Generates Key from random characters
 file = open("encryption_key.txt", 'wb')
 file.write(key)
 file.close()
